b60ecba66a2c6d26161510fc53c7a929354b2397
Lisa Wand <lisavenneker21@gmail.com>
2020-11-11 15:25:43 -0500

    commentrouter set

---
+++ routes/CommentRouter.js
@@ -0,0 +1,16 @@
+const { Router } = require('express')
+const router = Router()
+
+const CommentController = require('../controllers/CommentController')
+
+Router.post(
+    '/:post_id/user/:user_id', CommentController.CreateComment
+)
+Router.delete(
+    '/:post_id/remove/:comment_id', CommentController.RemoveComment
+)
+Router.put(
+    '/:comment_id', CommentController.UpdateComment
+)
+
+module.exports = router

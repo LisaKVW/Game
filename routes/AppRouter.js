8d5d678bc34daacf0bb5b201777d76519ae62548
Lisa Wand <lisavenneker21@gmail.com>
2020-11-11 15:07:24 -0500

    set up main Router AppRouter and 3 router in match with schemas

---
+++ routes/AppRouter.js
@@ -0,0 +1,13 @@
+const Router = require('express').Router()
+
+const UserRouter = require('./UserRouter')
+const CommentRouter = require('./CommentRouter')
+const PostingRouter = require('./PostingRouter')
+
+Router.use('/users', UserRouter)
+Router.use('/comments', CommentRouter)
+Router.use('/posts', PostingRouter)
+
+module.exports = Router
+
+//as we have 3 schema's - breaking down all routes in3 - and bringing them together ain AppRouter

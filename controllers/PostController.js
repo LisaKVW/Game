d417098b025331c5f5b292b96391022843ac10e8
Lisa Wand <lisavenneker21@gmail.com>
2020-11-11 13:00:45 -0500

    postcontroller done

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -14,3 +14,69 @@ const GetPosts = async (req, res) => {
         throw error
     }
 }
+
+const GetPostById = async (req, res) => {
+    try {
+        const post = await Posting.findById(req.params.post_id).populate([
+            {
+                model: 'users',
+                path: 'user_id',
+                select: '_id name'
+            },
+            {
+                path: 'content',
+                populate: {
+                    path: 'user_id',
+                    model: 'users',
+                    select: '_id name'
+                }
+            }
+        ])
+        res.send(post)
+    } catch (error) {
+        throw error
+    }
+}
+
+const CreatePost = async (req, res) => {
+    try {
+        const newPost = new Posting({ ...req.body, user_id: req.params.user_id })
+        newPost.save()
+        res.send(newPost)
+    } catch (error) {
+        throw error
+    }
+}
+
+const DeletePost = async (req, res) => {
+    try {
+        await Comment.deleteMany({ _id: { $in: post.comments } })
+        await Posting.findByIdAndDelete(req.params.post_id)
+        res.send({ msg: 'Post deleted' })
+    } catch (error) {
+        throw error
+    }
+}
+
+const UpdatePost = async (req, res) => {
+    try {
+        await Posting.findByIdAndUpdate(
+            req.params.post_id,
+            {
+                ...req.body
+            },
+            { new: true, useFindAndModify: false },
+            (err, (d) => (err ? err : res.send(d)))
+        )
+    } catch (error) {
+        throw error
+    }
+}
+
+module.exports = {
+    GetPosts,
+    GetPostById,
+    CreatePost,
+    DeletePost,
+    UpdatePost
+}

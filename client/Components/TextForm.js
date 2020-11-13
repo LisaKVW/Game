01f5271d52e2f9f60e8b4940114c29a482c40ae2
Lisa Wand <lisavenneker21@gmail.com>
2020-11-12 22:39:12 -0500

    textform component done

---
+++ client/Components/TextForm.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default (props) =>
+    props.fieldType === 'textfield' ? (  //ternary for a from, if true (empty textfield fill in the below)
+        <textarea                       //false when autcomplete is true (as) form is filled in
+            type={props.type}           //use textearea so the form shows some text in it
+            name={props.name}
+            value={props.value}
+            onChange={(e) => props.onChange(e)}
+            placeholder={props.placeholder}
+        />
+    ) : (
+            <input      //update form based on the input
+            type={props.type}
+            name={props.name}
+            value={props.value}
+            onChange={props.placeholder}
+            autoComplete="false"
+            />
+        )   

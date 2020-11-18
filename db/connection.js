3ce471a255caabb090f35cc113e0f8a17ec49fa7
Lisa Wand <lisavenneker21@gmail.com>
2020-11-17 20:11:53 -0500

    mongo atlas and heroku added

--- db/connection.js
+++ db/connection.js
@@ -1,6 +1,8 @@
+require('dotenv').config()
 const mongoose = require('mongoose')
+const MONGO_URI = process.env.MONGO_URI 
 
-const connection = mongoose.connect('mongodb://localhost:27017/Game-Insider', {
+const connection = mongoose.connect(process.env.NODE_ENV === 'production' ? MONGO_URI : 'mongodb://localhost:27017/Game-Insider', {
     useNewUrlParser: true,
     useFindAndModify: true,
     useUnifiedTopology: true,
@@ -11,4 +13,3 @@ mongoose.set('debug', true)
 
 module.exports = connection
 
-//connection to posting? this as this will be connecting new data - and comments will be posted to posts
--- server.js
+++ server.js
@@ -1,20 +1,26 @@
-const AppRouter = require('./routes/AppRouter')
 const express = require('express')
+const AppRouter = require('./routes/AppRouter')
 const logger = require('morgan')
 const cors = require('cors')
 const bodyParser = require('body-parser')
 const helmet = require('helmet')
 const connection = require('./db/connection')
+const path = require('path')
 
 const PORT = process.env.PORT || 3003
 const app = express()
 
 // Initialize Middleware
 app.use(logger('dev'))
-app.use(helmet())
+app.use(helmet({ contentSecurityPolicy: false }))
 app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.static(path.join(__dirname, 'client', 'build')))
+app.get('*', (req, res) =>
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+)
+
 
 // Initialize Middleware
 app.disable('X-Powered-By')
